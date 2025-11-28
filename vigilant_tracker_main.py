f4471074cf9c207ea3ba1a0b4864fbe18b52a375
Vignesh Thondikulam <vignesh.tho2006@gmail.com>
2025-11-28 08:42:15 -0800

    Removed commented out update array

--- vigilant_tracker_main.py
+++ vigilant_tracker_main.py
@@ -358,7 +358,6 @@ while (num > 0):
     # Display countdown timer
     print(f"\r{BOLD}{BRIGHT_MAGENTA}{BG_YELLOW}{int(num / 60)} minute(s) and {num % 60} second(s) remain{RESET}",end=" ")
     # Update sensor data
-    # updateArray(sensorArray, count, initialNum, num)
     count = updateArray(sensorArray, count, initialNum, num)
     aveTemp = updateAveTemp(aveTemp)
     aveSpeed = updateAveSpeed(aveSpeed)
